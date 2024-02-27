1a80bcc2be2e35df837f018e15b1926cc90395f9
Aleksandr <62632691+AleksandrElizarov@users.noreply.github.com>
2024-02-27 15:46:06 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 # Pin definition
-RST_PIN = 25 #Raspberry pin-22 к LCD_RST
-DC_PIN = 24  #Raspberry pin-18 к LCD_CS
-SPI_BUS = 0  #физическому пину номер 24
+RST_PIN = 25                            #Raspberry pin-22 к LCD_RST RESET
+DC_PIN = 24                             #Raspberry pin-18 к LCD_RS command/data(false/true)
+SPI_BUS = 0                             #Raspberry pin-24 к LCD_CS chip select, low level enable
+#MOSI (Master Out Slave In)             #Raspberry pin-19 к LCD_WR chip select, low level enable
+
 SPI_DEVICE = 0
