09b5f4586ab5f4d753d8973a93f4ef254b4c19cf
Aleksandr Elizarov <aleksandr.elizar@mail.ru>
2024-03-25 16:06:47 +0600

    Наполнение класса Coin Pulse

--- CoinPulse.py
+++ CoinPulse.py
@@ -14,25 +14,50 @@ class CoinPulse(object):
         self.credit_coin_list = [] #Список поступивших монет
         
         self.coin_running = False
-        self.duration_pulse = 0
+        self.count_pulse = 0
+        self.duration_pulses = 2 # Максимальная длительность в течение, которой ппоступают импульсы
         self.last_time_pulse = 0
+
+        self.start_time_from_pulse = 0
         
         # Инициализация GPIO
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
         # Настройка пина как вход
-        GPIO.setup(GPIO_board_port, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Пин монетоприемника
-        
+        GPIO.setup(self.GPIO_board_port, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Пин монетоприемника
         # Настройка прерывания
-        GPIO.add_event_detect(GPIO_board_port, GPIO.FALLING, callback=count_coin, bouncetime=100)
+        GPIO.add_event_detect(self.GPIO_board_port, GPIO.FALLING, callback=self.count_coin, bouncetime=100)
+
+        system_loop_thread = threading.Thread(target=self.system_loop)
+        system_loop_thread.setDaemon(True)
+        system_loop_thread.start()
                         
     #Функция которая вызывается по прерыванию от монетоприемника
-    def count_coin(channel):
-       
-        print(f'PULSE: {time.time() - last_time_pulse}')
-        last_time_pulse = time.time()
-        coin_counter += 1
-        print(f"Количество импульсов: {coin_counter}")        
+    def count_coin(self, channel):
+        if(self.coin_running == False):
+            self.coin_running = True
+            self.start_time_from_pulse = time.time()
+        self.count_pulse += 1
+        print(f"Количество импульсов: {self.count_pulse}")    
+
+    def system_loop(self):  
+        while True:
+            if(self.coin_running):
+                if(time.time() - self.start_time_from_pulse >= self.duration_pulses):
+                    print(f"Поступила монета номиналом: {self.count_pulse}")
+                    self.credit_coin_list.append(self.count_pulse)
+                    self.start_time_from_pulse = 0
+                    self.count_pulse = 0
+                    self.coin_running = False
+
+    def get_last_credit_cash_from_list(self):
+        pass
+
+                    
+
+
+
+                    
             
 
         
